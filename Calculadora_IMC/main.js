1f646eb6e9fd035b9decb9557fc84257287fff07
MichaelOliveira13 <micayl95@gmail.com>
2022-08-24 20:11:29 -0300

    First commit

---
+++ Calculadora_IMC/main.js
@@ -0,0 +1,11 @@
+const calcular = document.getElementById('calcular');
+const nome = document.getElementById('input-nome');
+const altura = document.getElementById('input-altura');
+const peso = document.getElementById('input-peso');
+const resultado = document.getElementById('result');
+
+function imc (){
+    alert ('teste do click');
+}
+
+calcular.addEventListener('click', imc);
