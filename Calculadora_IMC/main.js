9541f3ec8a70e83ba05ba2e6be3c25e4e4e1afd1
MichaelOliveira13 <micayl95@gmail.com>
2022-08-24 21:47:16 -0300

    Incrementando JS

--- Calculadora_IMC/main.js
+++ Calculadora_IMC/main.js
@@ -1,11 +1,42 @@
-const calcular = document.getElementById('calcular');
-const nome = document.getElementById('input-nome');
-const altura = document.getElementById('input-altura');
-const peso = document.getElementById('input-peso');
-const resultado = document.getElementById('result');
+const calcular = document.getElementById('calcular')
+calcular.addEventListener('click', op);
 
-function imc (){
-    alert ('teste do click');
-}
+function op (){
 
-calcular.addEventListener('click', imc);
+    const nome = document.getElementById('input-nome').value;
+    const altura = document.getElementById('input-altura').value;
+    const peso = document.getElementById('input-peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !==''){
+
+        const imc = (peso / (altura * altura)).toFixed(2);
+
+        let parcial = '';
+
+        if (imc < 19)
+        {parcial = 'abaixo do peso.'}
+
+        else if (imc > 19 && imc <= 25)
+        {parcial = 'no peso ideal.'}
+
+        else if (imc > 25 && imc <= 30)
+        {parcial = 'com sobrepeso.'}
+
+        else if (imc > 30 && imc <= 35)
+        {parcial = 'com obesidade moderada.'}
+
+        else if (imc > 35 && imc <= 40)
+        {parcial = 'com obesidade severa.'}
+
+        else if(imc > 40 && imc <= 50)
+        {parcial = 'com obesidade morbida.'}
+
+        resultado.textContent = `${nome} seu IMC é ${imc} e você está ${parcial}`;
+        }
+        else
+        {resultado.textContent = 'Preencha os campos corretamente!'}
+    }
+
+
+
